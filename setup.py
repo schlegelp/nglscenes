0f7ce0b994ae585fc72ab16eb823ca0a899226c7
Philipp Schlegel <observing@web.de>
2025-06-13 09:29:55 +0100

    drop support for Python 3.7 + 3.8 and start testing against 3.10-12

--- setup.py
+++ setup.py
@@ -45,14 +45,13 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     install_requires=requirements,
-    # CI runs against >=3.7
-    # but R-Python interface ships with 3.6 so this is necessary
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     zip_safe=False,
 
     include_package_data=True
