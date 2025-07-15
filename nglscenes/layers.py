0867e3ef246496f1170753c44aac7905f90d7992
Philipp Schlegel <observing@web.de>
2025-07-15 10:24:09 +0100

    fix issue in LayerManager when not run from a terminal

--- nglscenes/layers.py
+++ nglscenes/layers.py
@@ -480,18 +480,21 @@ class LayerManager:
         self.scene = scene
 
     def __str__(self):
-        return f'LayerManager<{len(self)} layers>'
+        return f"LayerManager<{len(self)} layers>"
 
     def __repr__(self):
-        twidth = os.get_terminal_size().columns
+        try:
+            twidth = os.get_terminal_size().columns
+        except OSError:
+            twidth = 80
         lstr = []
         for i, l in enumerate(self.scene.layers):
             s = f"{i}: {l}"
             if len(s) > twidth:
-                s = s[:(twidth // 2) - 2] + '...' + s[-((twidth // 2) - 2):]
+                s = s[: (twidth // 2) - 2] + "..." + s[-((twidth // 2) - 2) :]
             lstr.append(s)
         lstr = "\n".join(lstr)
-        return f'LayerManager with {len(self)} layers:\n{lstr}'
+        return f"LayerManager with {len(self)} layers:\n{lstr}"
 
     def __len__(self):
         return len(self.scene._layers)
