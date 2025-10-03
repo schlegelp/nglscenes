cf0533a8ebaa3e5f1f65a35395eae6df128d50a8
Philipp Schlegel <observing@web.de>
2025-10-03 15:52:13 +0100

    examples: add HemibrainScene and MancScene

--- nglscenes/examples.py
+++ nglscenes/examples.py
@@ -17,91 +17,190 @@
 # - FAFB
 # - hemibrain
 
-from .local import LocalScene
+from .local import LocalScene, Scene
 from .layers import ImageLayer, SegmentationLayer, AnnotationLayer
 from .graphene import FlyWireSegmentationLayer, FancSegmentationLayer
 
-__all__ = ['FAFBScene', 'FancScene', 'FlyWireScene']
+__all__ = ["FAFBScene", "FancScene", "FlyWireScene", "HemibrainScene", "MancScene"]
 
 
 class FAFBScene(LocalScene):
     """NeuroGlancer scene containing FAFB data (image, segmentation, synapses)."""
+
     def __init__(self):
         super().__init__()
 
         # Add image layer
-        self.add_layers(ImageLayer(source='precomputed://gs://neuroglancer-fafb-data/fafb_v14/fafb_v14_clahe',
-                                   name='fafb_v14_clahe'))
+        self.add_layers(
+            ImageLayer(
+                source="precomputed://gs://neuroglancer-fafb-data/fafb_v14/fafb_v14_clahe",
+                name="fafb_v14_clahe",
+            )
+        )
 
         # Add segmentation layer
-        self.add_layers(SegmentationLayer(source="precomputed://gs://fafb-ffn1-20200412/segmentation",
-                                          name='fafb-ffn1-20200412',
-                                          segments=["710435991"]))
+        self.add_layers(
+            SegmentationLayer(
+                source="precomputed://gs://fafb-ffn1-20200412/segmentation",
+                name="fafb-ffn1-20200412",
+                segments=["710435991"],
+            )
+        )
 
         # Add annotation layer
-        self.add_layers(AnnotationLayer(source="precomputed://gs://neuroglancer-20191211_fafbv14_buhmann2019_li20190805",
-                                        linkedSegmentationLayer={"pre_segment": "fafb-ffn1-20200412",
-                                                                 "post_segment": "fafb-ffn1-20200412"},
-                                        annotationColor="#cecd11",
-                                        shader="#uicontrol vec3 preColor color(default=\"blue\")\n#uicontrol vec3 postColor color(default=\"red\")\n#uicontrol float scorethr slider(min=0, max=1000)\n#uicontrol int showautapse slider(min=0, max=1)\n\nvoid main() {\n  setColor(defaultColor());\n  setEndpointMarkerColor(\n    vec4(preColor, 0.5),\n    vec4(postColor, 0.5));\n  setEndpointMarkerSize(5.0, 5.0);\n  setLineWidth(2.0);\n  if (int(prop_autapse()) > showautapse) discard;\n  if (prop_score()<scorethr) discard;\n}\n\n",
-                                        shaderControls={"scorethr": 80},
-                                        filterBySegmentation=["post_segment",
-                                                              "pre_segment"],
-                                        name='synapses_buhmann2019'))
+        self.add_layers(
+            AnnotationLayer(
+                source="precomputed://gs://neuroglancer-20191211_fafbv14_buhmann2019_li20190805",
+                linkedSegmentationLayer={
+                    "pre_segment": "fafb-ffn1-20200412",
+                    "post_segment": "fafb-ffn1-20200412",
+                },
+                annotationColor="#cecd11",
+                shader='#uicontrol vec3 preColor color(default="blue")\n#uicontrol vec3 postColor color(default="red")\n#uicontrol float scorethr slider(min=0, max=1000)\n#uicontrol int showautapse slider(min=0, max=1)\n\nvoid main() {\n  setColor(defaultColor());\n  setEndpointMarkerColor(\n    vec4(preColor, 0.5),\n    vec4(postColor, 0.5));\n  setEndpointMarkerSize(5.0, 5.0);\n  setLineWidth(2.0);\n  if (int(prop_autapse()) > showautapse) discard;\n  if (prop_score()<scorethr) discard;\n}\n\n',
+                shaderControls={"scorethr": 80},
+                filterBySegmentation=["post_segment", "pre_segment"],
+                name="synapses_buhmann2019",
+            )
+        )
 
 
 class FlyWireScene(LocalScene):
     """NeuroGlancer scene containing FlyWire data (meshes, FAFB14 image data)."""
+
     def __init__(self):
         super().__init__()
 
         # Add image layer
-        self.add_layers(ImageLayer(source='precomputed://https://bossdb-open-data.s3.amazonaws.com/flywire/fafbv14',
-                                   name='fafb_v14'))
+        self.add_layers(
+            ImageLayer(
+                source="precomputed://https://bossdb-open-data.s3.amazonaws.com/flywire/fafbv14",
+                name="fafb_v14",
+            )
+        )
 
         # Add FlyWire mesh layer
         self.add_layers(FlyWireSegmentationLayer(segments=["720575940621039145"]))
 
         # Add FAFB mesh
-        self.add_layers(SegmentationLayer(source="precomputed://gs://flywire_neuropil_meshes/whole_neuropil/brain_mesh_v141.surf",
-                                          name='fafb-neuropil',
-                                          selectedAlpha=0,
-                                          objectAlpha=0.05,
-                                          segmentColor={"1": "#ffffff"},
-                                          segments=["1"]))
+        self.add_layers(
+            SegmentationLayer(
+                source="precomputed://gs://flywire_neuropil_meshes/whole_neuropil/brain_mesh_v141.surf",
+                name="fafb-neuropil",
+                selectedAlpha=0,
+                objectAlpha=0.05,
+                segmentColor={"1": "#ffffff"},
+                ignoreSegmentInteractions=True,
+                segments=["1"],
+            )
+        )
 
         # For some reason it won't work if try I initializing the scene with
         # these settings
-        self.state.update({"position": [538699, 221231, 131019],
-                           "dimensions": {"x": [1e-9, "m"],
-                                          "y": [1e-9, "m"],
-                                          "z": [1e-9, "m"]},
-                           "layout": "3d",
-                           "showSlices": False,
-                           "projectionScale": 365017})
+        self.state.update(
+            {
+                "position": [538699, 221231, 131019],
+                "dimensions": {"x": [1e-9, "m"], "y": [1e-9, "m"], "z": [1e-9, "m"]},
+                "layout": "3d",
+                "showSlices": False,
+                "projectionScale": 365017,
+            }
+        )
         self.push_state()
 
 
 class FancScene(LocalScene):
     """NeuroGlancer scene containing FANC data."""
+
     def __init__(self):
         super().__init__()
 
         # Add image layer
-        self.add_layers(ImageLayer(source='precomputed://gs://zetta_lee_fly_vnc_001_precomputed/fanc_v4_em',
-                                   name='FANC_v4 '))
+        self.add_layers(
+            ImageLayer(
+                source="precomputed://gs://zetta_lee_fly_vnc_001_precomputed/fanc_v4_em",
+                name="FANC_v4 ",
+            )
+        )
 
         # Add FANC layer
-        self.add_layers(FancSegmentationLayer(segments=['648518346478550356',
-                                                        '648518346476465526']))
+        self.add_layers(
+            FancSegmentationLayer(segments=["648518346478550356", "648518346476465526"])
+        )
 
         # For some reason it won't work if try I initializing the scene with
         # these settings
-        self.state.update({"position": [176052, 478389, 132651],
-                           "dimensions": {"x": [1e-9, "m"],
-                                          "y": [1e-9, "m"],
-                                          "z": [1e-9, "m"]},
-                           "layout": "3d",
-                           "showSlices": False,
-                           "projectionScale": 1095841})
+        self.state.update(
+            {
+                "position": [176052, 478389, 132651],
+                "dimensions": {"x": [1e-9, "m"], "y": [1e-9, "m"], "z": [1e-9, "m"]},
+                "layout": "3d",
+                "showSlices": False,
+                "projectionScale": 1095841,
+            }
+        )
         self.push_state()
+
+
+class HemibrainScene(Scene):
+    """NeuroGlancer scene containing Hemibrain data."""
+
+    def __init__(self):
+        super().__init__()
+
+        # Add image layer
+        self.add_layers(
+            ImageLayer(
+                source="precomputed://gs://neuroglancer-janelia-flyem-hemibrain/emdata/clahe_yz/jpeg",
+                name="hemibrain:v1.2.1-grayscalejpeg ",
+            )
+        )
+
+        # Add segmentation layer
+        self.add_layers(
+            SegmentationLayer(
+                source="precomputed://gs://neuroglancer-janelia-flyem-hemibrain/v1.2/segmentation"
+            )
+        )
+
+        # Add synapse layer
+        self.add_layers(
+            AnnotationLayer(
+                source="precomputed://gs://neuroglancer-janelia-flyem-hemibrain/v1.2/synapses",
+                shader='#uicontrol vec3 preColor color(default="yellow")\n#uicontrol vec3 postColor color(default="gray")\n#uicontrol float preConfidence slider(min=0, max=1, default=0)\n#uicontrol float postConfidence slider(min=0, max=1, default=0)\n#uicontrol float sliceViewOpacity slider(min=0, max=1, default=0.5)\n#uicontrol float projectionViewOpacity slider(min=0, max=1, default=0.3)\n\nvoid main() {\n  float opacity = PROJECTION_VIEW ? projectionViewOpacity : sliceViewOpacity;\n  setColor(vec4(defaultColor(), opacity));\n  setEndpointMarkerColor(\n    vec4(preColor, opacity),\n    vec4(postColor, opacity));\n  setEndpointMarkerBorderColor(\n    vec4(0, 0, 0, opacity),\n    vec4(0, 0, 0,     opacity)\n  );\n\n  setEndpointMarkerSize(5.0, 5.0);\n  setLineWidth(2.0);\n  if (prop_pre_synaptic_confidence()< preConfidence ||\n  prop_post_synaptic_confidence()< postConfidence) discard;\n}\n',
+                linkedSegmentationLayer={
+                    "pre_synaptic_cell": "hemibrain:v1.2.1",
+                    "post_synaptic_cell": "hemibrain:v1.2.1",
+                },
+                filterBySegmentation=["post_synaptic_cell", "pre_synaptic_cell"],
+                name="hemibrain:v1.2.1-synapses",
+            )
+        )
+
+        # Same settings as in neuPrint
+        self.state.update(
+            {
+                "dimensions": {"x": [8e-9, "m"], "y": [8e-9, "m"], "z": [8e-9, "m"]},
+                "position": [3761, 19530, 13253],
+                "crossSectionScale": 1,
+                "projectionOrientation": [
+                    0.10976426303386688,
+                    -0.32929277420043945,
+                    -0.32929277420043945,
+                    0.8781141042709351,
+                ],
+                "projectionScale": 983.7572106361007,
+            }
+        )
+
+
+class MancScene(Scene):
+    """NeuroGlancer scene showing male adult nerve cord (MANC) data."""
+
+    def __new__(self):
+        return (
+            super()
+            .__new__(self)
+            .from_url(
+                "https://neuroglancer-demo.appspot.com/#!gs://flyem-user-links/short/2024-10-22.084840.490679.json"
+            )
+        )
