b9540767c04d6f1097299a5afda15eabdeb8e2d9
Philipp Schlegel <observing@web.de>
2026-07-16 14:04:24 +0100

    swap matplotlib for cmap

--- nglscenes/layers.py
+++ nglscenes/layers.py
@@ -21,20 +21,31 @@ from abc import ABC
 from collections import OrderedDict
 
 import numpy as np
-import matplotlib.colors as mcl
+import pandas as pd
+from cmap import Color, Colormap
 
 from .utils import add_on_change_callback, remove_callback
 
-__all__ = ['ImageLayer', 'SegmentationLayer', 'AnnotationLayer', 'MeshLayer']
+__all__ = ["ImageLayer", "SegmentationLayer", "AnnotationLayer", "MeshLayer"]
 
 logger = logging.getLogger(__name__)
+
+
+def _to_hex(c):
+    """Convert color (name, hex or RGB(A) sequence) to "#rrggbb" hex code."""
+    if isinstance(c, (tuple, list, np.ndarray)):
+        # `cmap` interprets integers as being in the 0-255 range but we
+        # expect RGB values to be in the 0-1 range
+        c = tuple(float(v) for v in c)
+    # Slicing off any alpha channel
+    return Color(c).hex[:7].lower()
 
 
 class BaseLayer(ABC):
     """Abstract base class for layers."""
 
     # Defines required properties (at initialization)
-    MUST_HAVE = ['name']
+    MUST_HAVE = ["name"]
     MUST_NOT_HAVE = []
     MUST_ONLY_HAVE = []
 
@@ -56,12 +67,12 @@ class BaseLayer(ABC):
         self.validate_properties()
 
     def __init_layer__(self):
-        return self.NG_LAYER(source=self['source'])
+        return self.NG_LAYER(source=self["source"])
 
     @property
     def name(self):
         # This is the unique identifier for this layer
-        return self._state['name']
+        return self._state["name"]
 
     @property
     def state(self):
@@ -73,7 +84,7 @@ class BaseLayer(ABC):
     @state.setter
     def state(self, value):
         if not isinstance(value, dict):
-            raise TypeError('State must be a dictionary')
+            raise TypeError("State must be a dictionary")
         if self._viewer:
             value = add_on_change_callback(value, callback=self.push_state)
         self._state = value
@@ -94,7 +105,7 @@ class BaseLayer(ABC):
         return True
 
     def __str__(self):
-        return f'<{self.type}(name={self.name}, source={self.state["source"]})>'
+        return f"<{self.type}(name={self.name}, source={self.state['source']})>"
 
     def __repr__(self):
         return self.__str__()
@@ -104,9 +115,9 @@ class BaseLayer(ABC):
         state = dict(self.__dict__)
 
         # Set viewer to None if present
-        if '_viewer' in state:
-            state['_viewer'] = None
-            state['_state'] = remove_callback(state['_state'])
+        if "_viewer" in state:
+            state["_viewer"] = None
+            state["_state"] = remove_callback(state["_state"])
 
         return state
 
@@ -140,10 +151,10 @@ class BaseLayer(ABC):
 
     def __or__(self, other):
         if type(other) is not type(self):
-            raise NotImplementedError(f'Unable to combine {type(other)} with '
-                                      f'{self.type}')
-        raise NotImplementedError(f'Combination not implemented for {self.type}')
-
+            raise NotImplementedError(
+                f"Unable to combine {type(other)} with {self.type}"
+            )
+        raise NotImplementedError(f"Combination not implemented for {self.type}")
 
     def as_dict(self):
         """Return dictionary describing this layer."""
@@ -180,51 +191,59 @@ class BaseLayer(ABC):
         # Push state to viewer
         self.push_state()
 
-    def push_state(self, on_error='raise'):
+    def push_state(self, on_error="raise"):
         """Push state to neuroglancer viewer."""
         if self._lock:
-            logger.debug('Pushing state aborted: Layer locked')
+            logger.debug("Pushing state aborted: Layer locked")
             return
 
         if not self.viewer:
-            raise ValueError('Layer is not linked to a neuroglancer viewer.')
+            raise ValueError("Layer is not linked to a neuroglancer viewer.")
 
         with self.viewer.txn() as s:
             state = s.to_json()
-            ix = [i for i, l in enumerate(state.get('layers', [])) if l.get('name', None) == self.name]
+            ix = [
+                i
+                for i, l in enumerate(state.get("layers", []))
+                if l.get("name", None) == self.name
+            ]
             if not ix:
-                raise ValueError(f'Layer "{self.name}" not found in '
-                                 'neuroglancer viewer.')
+                raise ValueError(
+                    f'Layer "{self.name}" not found in neuroglancer viewer.'
+                )
             elif len(ix) > 1:
                 raise ValueError(f'Layer "{self.name}" duplicated.')
 
             # Update state
-            state['layers'][ix[0]].update(self._state)
+            state["layers"][ix[0]].update(self._state)
 
         self.viewer.set_state(state)
 
-        logger.debug(f'State pushed from layer: {state}')
+        logger.debug(f"State pushed from layer: {state}")
 
-    def pull_state(self, on_error='raise'):
+    def pull_state(self, on_error="raise"):
         """Pull state from neuroglancer viewer."""
         if self._lock:
-            logger.debug('Pulling state aborted: Layer locked')
+            logger.debug("Pulling state aborted: Layer locked")
             return
 
         if not self.viewer:
-            raise ValueError('Layer is not linked to a neuroglancer viewer.')
+            raise ValueError("Layer is not linked to a neuroglancer viewer.")
 
         with self.viewer.txn() as s:
             state = s.to_json()
-            layer = [l for l in state.get('layers', []) if l.get('name', None) == self.name]
+            layer = [
+                l for l in state.get("layers", []) if l.get("name", None) == self.name
+            ]
             if not layer:
-                raise ValueError(f'Layer "{self.name}" not found in '
-                                 'neuroglancer viewer.')
+                raise ValueError(
+                    f'Layer "{self.name}" not found in neuroglancer viewer.'
+                )
             elif len(layer) > 1:
                 raise ValueError(f'Layer "{self.name}" duplicated.')
             self.state = layer[0]
 
-        logger.debug(f'State pulled from layer: {state}')
+        logger.debug(f"State pulled from layer: {state}")
 
     def validate_properties(self):
         """Check state for missing/forbidden values."""
@@ -243,28 +262,34 @@ class BaseLayer(ABC):
 class ImageLayer(BaseLayer):
     """Image layer."""
 
-    DEFAULTS = OrderedDict({'source': '',
-                            'type': 'image',
-                            'blend': 'default',
-                            'shaderControls': {},
-                            'name': 'img'})
-    MUST_HAVE = ['name', 'source']
+    DEFAULTS = OrderedDict(
+        {
+            "source": "",
+            "type": "image",
+            "blend": "default",
+            "shaderControls": {},
+            "name": "img",
+        }
+    )
+    MUST_HAVE = ["name", "source"]
 
     NG_LAYER = neuroglancer.ImageLayer
 
     def __init__(self, source, **kwargs):
         props = copy.deepcopy(self.DEFAULTS)
-        props['source'] = source
+        props["source"] = source
         props.update(**kwargs)
         super().__init__(**props)
 
     def __or__(self, other):
         if type(other) is not type(self):
-            raise NotImplementedError(f'Unable to combine {type(other)} with '
-                                      f'{self.type}')
-        if self['source'] != other['source']:
-            raise NotImplementedError('Unable to combine image layers with '
-                                      'different sources')
+            raise NotImplementedError(
+                f"Unable to combine {type(other)} with {self.type}"
+            )
+        if self["source"] != other["source"]:
+            raise NotImplementedError(
+                "Unable to combine image layers with different sources"
+            )
         x = copy.deepcopy(self)
         x.state.update(other.state)
         return x
@@ -273,56 +298,64 @@ class ImageLayer(BaseLayer):
 class SegmentationLayer(BaseLayer):
     """Segmentation layer."""
 
-    DEFAULTS = OrderedDict({'source': '',
-                            'type': 'segmentation',
-                            'selectedAlpha': 0.14,
-                            'segments': [],
-                            'name': 'segmentation'})
-    MUST_HAVE = ['name', 'source']
+    DEFAULTS = OrderedDict(
+        {
+            "source": "",
+            "type": "segmentation",
+            "selectedAlpha": 0.14,
+            "segments": [],
+            "name": "segmentation",
+        }
+    )
+    MUST_HAVE = ["name", "source"]
 
     STATE_CONVERSION = {
-        'segments': {
-            int : lambda x: [str(x)],
-            str : lambda x: [x],
-            list : lambda x: [str(s) for s in x],
-            np.ndarray: lambda x: x.astype(str).tolist()
+        "segments": {
+            int: lambda x: [str(x)],
+            str: lambda x: [x],
+            list: lambda x: [str(s) for s in x],
+            np.ndarray: lambda x: x.astype(str).tolist(),
         },
-        'segmentColors': {
-            dict: lambda x: {str(k): mcl.to_hex(v) for k, v in x.items()}
+        "segmentColors": {
+            dict: lambda x: {str(k): _to_hex(v) for k, v in x.items()}
         },
         "segmentDefaultColor": {
-            str: lambda x: mcl.to_hex(x),
-            tuple: lambda x: mcl.to_hex(x),
-            np.ndarray: lambda x: mcl.to_hex(x)
-        }
+            str: lambda x: _to_hex(x),
+            tuple: lambda x: _to_hex(x),
+            np.ndarray: lambda x: _to_hex(x),
+        },
     }
 
     NG_LAYER = neuroglancer.SegmentationLayer
 
     def __init__(self, source, **kwargs):
         if isinstance(source, str):
-            if source.startswith('graphene') and 'middleauth' not in source:
-                logger.warning('Looks like you want to generate segmentation layer '
-                            'with a graphene (chunkedgraph) source. The '
-                            'recommended way of doing this is using the '
-                            'nglscenes.GrapheneSegmentationLayer class instead '
-                            'of a basic SegmentationLayer.')
+            if source.startswith("graphene") and "middleauth" not in source:
+                logger.warning(
+                    "Looks like you want to generate segmentation layer "
+                    "with a graphene (chunkedgraph) source. The "
+                    "recommended way of doing this is using the "
+                    "nglscenes.GrapheneSegmentationLayer class instead "
+                    "of a basic SegmentationLayer."
+                )
 
         props = copy.deepcopy(self.DEFAULTS)
-        props['source'] = source
+        props["source"] = source
         props.update(**kwargs)
         super().__init__(**props)
 
     def __or__(self, other):
         if type(other) is not type(self):
-            raise NotImplementedError(f'Unable to combine {type(other)} with '
-                                      f'{self.type}')
-        if self['source'] != other['source']:
-            raise NotImplementedError('Unable to combine segmentation layers '
-                                      'with different sources')
+            raise NotImplementedError(
+                f"Unable to combine {type(other)} with {self.type}"
+            )
+        if self["source"] != other["source"]:
+            raise NotImplementedError(
+                "Unable to combine segmentation layers with different sources"
+            )
         x = copy.deepcopy(self)
         # Combine selected segments
-        x['segments'] = list(set(x['segments'] + other['segments']))
+        x["segments"] = list(set(x["segments"] + other["segments"]))
         return x
 
     def __setitem__(self, name, value):
@@ -330,14 +363,14 @@ class SegmentationLayer(BaseLayer):
         super().__setitem__(name, value)
 
     def __str__(self):
-        source = self['source']
+        source = self["source"]
         if isinstance(source, list):
             source = source[0]
 
         if isinstance(source, dict):
-            source = source.get('url', source)
+            source = source.get("url", source)
 
-        return f'<{self.type}(name={self.name}, source={source}, selected segments={len(self.get("segments", []))})>'
+        return f"<{self.type}(name={self.name}, source={source}, selected segments={len(self.get('segments', []))})>"
 
     def add_subsource(self, subsource):
         """Add a subsource to the layer.
@@ -373,7 +406,7 @@ class SegmentationLayer(BaseLayer):
         """
         assert isinstance(x, (str, tuple, list, dict, np.ndarray))
 
-        segments = self.get('segments', [])
+        segments = self.get("segments", [])
 
         # Parse color(s)
         # 1. Single color (e.g. "white")
@@ -385,20 +418,20 @@ class SegmentationLayer(BaseLayer):
         # 3. A (N, ) list of labels
         elif isinstance(x, (np.ndarray, list)):
             if len(x) != len(segments):
-                raise ValueError(
-                    f"Got {len(x)} colors for {len(segments)} segments."
-                )
+                raise ValueError(f"Got {len(x)} colors for {len(segments)} segments.")
 
             seg_colors = dict(zip(segments, x))
         elif isinstance(x, dict):
             seg_colors = x
         else:
-            raise TypeError('Colors must be strings, RGB tuples, a list thereof '
-                            f'or a dictionary. Got {type(x)}')
+            raise TypeError(
+                "Colors must be strings, RGB tuples, a list thereof "
+                f"or a dictionary. Got {type(x)}"
+            )
 
         # Turn colors into hex codes
         # Also make sure keys are strings
-        seg_colors = {str(s): mcl.to_hex(c) for s, c in seg_colors.items()}
+        seg_colors = {str(s): _to_hex(c) for s, c in seg_colors.items()}
 
         if "segmentColors" not in self:
             self["segmentColors"] = {}
@@ -415,8 +448,10 @@ class SegmentationLayer(BaseLayer):
                 Property to color by. Must be the same length as the number of
                 selected segments.
         palette :   str | dict
-                Name of the matplotlib or seaborn color palette to use.
-                Alternatively, a dictionary mapping property values to colors.
+                Name of a color palette (e.g. "tab10" or "viridis") - anything
+                the `cmap` package recognizes works, which includes matplotlib
+                and seaborn palettes. Alternatively, a dictionary mapping
+                property values to colors.
 
         """
         x = np.asarray(x)
@@ -431,7 +466,7 @@ class SegmentationLayer(BaseLayer):
 
         # Parse palette
         if isinstance(palette, str):
-            palette = plt.get_cmap(palette)
+            palette = Colormap(palette)
             colors = dict(zip(values, palette(np.linspace(0, 1, len(values)))))
         elif isinstance(palette, dict):
             if not all(v in palette for v in values):
@@ -444,7 +479,7 @@ class SegmentationLayer(BaseLayer):
 
         # Assign colors
         self["segmentColors"] = {
-            str(s): mcl.to_hex(colors[v]) for s, v in zip(self["segments"], x)
+            str(s): _to_hex(colors[v]) for s, v in zip(self["segments"], x)
         }
 
     def clear_colors(self):
@@ -459,10 +494,10 @@ class SegmentationLayer(BaseLayer):
 
         """
         # Segment Query is a string
-        query = [s.strip() for s in self.get('segmentQuery', '').split(',')]
-        sel = self.get('segments', [])
+        query = [s.strip() for s in self.get("segmentQuery", "").split(",")]
+        sel = self.get("segments", [])
 
-        self['segments'] = [s for s in query if s not in sel]
+        self["segments"] = [s for s in query if s not in sel]
 
 
 class AnnotationLayer(BaseLayer):
@@ -582,8 +617,8 @@ class AnnotationLayer(BaseLayer):
             df["z"] = [a["point"][2] for a in self.state["annotations"]]
         else:
             raise NotImplementedError(
-                f'Converting {self.state.get("tool", "NA")} annotations '
-                ' to pandas DataFrame currently not supported'
+                f"Converting {self.state.get('tool', 'NA')} annotations "
+                " to pandas DataFrame currently not supported"
             )
 
         return df
@@ -592,16 +627,14 @@ class AnnotationLayer(BaseLayer):
 class MeshLayer(BaseLayer):
     """Mesh layer."""
 
-    DEFAULTS = OrderedDict({'source': '',
-                            'type': 'mesh',
-                            'name': 'meshes'})
-    MUST_HAVE = ['name', 'source']
+    DEFAULTS = OrderedDict({"source": "", "type": "mesh", "name": "meshes"})
+    MUST_HAVE = ["name", "source"]
 
     NG_LAYER = neuroglancer.SingleMeshLayer
 
     def __init__(self, source, **kwargs):
         props = copy.deepcopy(self.DEFAULTS)
-        props['source'] = source
+        props["source"] = source
         props.update(**kwargs)
         super().__init__(**props)
 
@@ -634,12 +667,13 @@ class LayerManager:
         return self.get_layer(layer)
 
     def __setitem__(self, key, value):
-        raise NotImplementedError('Please use Scene.add_layer layers.')
+        raise NotImplementedError("Please use Scene.add_layer layers.")
 
     def __iter__(self):
         """Iterator instanciates a new class every time it is called.
         This allows the use of nested loops on the same layer object.
         """
+
         class prange_iter:
             def __init__(self, layers, start):
                 self.iter = start
@@ -665,7 +699,7 @@ class LayerManager:
 
     def get_layer(self, layer):
         if isinstance(layer, str):
-            l = [l for l in self.scene._layers if l._state.get('name', None) == layer]
+            l = [l for l in self.scene._layers if l._state.get("name", None) == layer]
             if not l:
                 raise AttributeError(f'No layer called "{layer}" found.')
             elif len(l) > 1:
